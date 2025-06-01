4c3ce4e416d1bdd4a835f58013b9fb395935cf90
abhitech2002 <oabhishek31@gmail.com>
2025-06-01 21:46:44 +0530

    Add authentication routes for user registration, login, and profile retrieval

--- server/index.js
+++ server/index.js
@@ -2,8 +2,11 @@ import express from 'express';
 import dotenv from 'dotenv';
 dotenv.config();
 import connectDB from './config/db.js';
+import authRoutes from './routes/authRoutes.js';
 
 const app = express();
+app.use(express.json());
+app.use('/api/v1/auth', authRoutes);
 
 const PORT = process.env.PORT || 5000;
 
