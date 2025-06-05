8837b9d673144b7ea5a72446b237f5e0542d2f74
abhitech2002 <oabhishek31@gmail.com>
2025-06-05 20:37:04 +0530

    feat: add axios and jwt-decode dependencies, implement api service for API calls

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,18 @@
-import './App.css'
-import {BrowserRouter, Routes, Route} from 'react-router-dom';
-import HomePage from './pages/HomePage';
-import LoginPage from './pages/LoginPage';
-import RegisterPage from './pages/RegisterPage';
-import AboutPage from './pages/AboutPage';
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
+import AboutPage from "./pages/AboutPage";
+import ProtectedRoute from "./components/ProtectedRoute";
+import { useAuth } from "./context/AuthContext";
+import Loader from "./components/Loader";
 
 function App() {
+  const { loading } = useAuth();
+  if (loading) {
+    return <Loader />;
+  }
 
   return (
     <BrowserRouter>
